4f9f197b5c6155bb3818168dfa03b13d4f44eab6
Peter <petersolomon704@gmail.com>
2023-02-18 17:50:09 +0100

    Update store.js
    Added saga configuration

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,15 @@
 // import { createStore } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
 import { rootReducer } from "./reducers/rotReducer";
+import createSagaMiddleWare from "redux-saga";
+import productSaga from "./saga/productSaga";
 
-export const store = configureStore({ reducer: rootReducer });
+const sagaMiddleware = createSagaMiddleWare();
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: () => [sagaMiddleware],
+});
+sagaMiddleware.run(productSaga);
 
 // export const store = createStore(rootReducer);
